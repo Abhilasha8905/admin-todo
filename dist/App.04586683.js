6e13f64ebce9967bf0056cdb2619db0aec77b06d
Abhilasha <abhilashajha41@gmail.com>
2020-02-17 13:28:42 +0530

    added changes for build

--- dist/App.04586683.js
+++ dist/App.04586683.js
@@ -50051,7 +50051,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51383" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57887" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
